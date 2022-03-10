48cca1888998069da1609343484d8e62f82ec425
emil237 <95539537+emil237@users.noreply.github.com>
2022-03-10 20:45:47 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,60 +1,22 @@
 #!/bin/sh
-#
-
+ # 
 #command wget https://raw.githubusercontent.com/emil237/scripts/main/installer.sh -O - | /bin/sh
-
-wget -O /var/volatile/tmp/script.tar.gz "https://raw.githubusercontent.com/emil237/scripts/main/script.tar.gz"
+ # # 
+cd /tmp
+set -e 
+wget "https://raw.githubusercontent.com/emil237/scripts/main/script.tar.gz"
 wait
-tar xzvpf /tmp/*.tar.gz  -C /
+tar -xzf script.tar.gz  -C /
 wait
-rm -r /var/volatile/tmp/script.tar.gz                         
-
-echo "========================================================================"
-echo " chmod  all scripts "
-
-chmod 755 /usr/script/*.sh
-                
-	echo ".        WELCOME TO SCRIPTS UPDATE "
-	
-echo "#    SCRIPT INSTALLED SUCCESSFULLY           #"
-echo "        UPLOADED BY EMIL_NABIL  "     
-echo "#########################################################"
-echo "#########################################################"
+cd ..
+set +e
+rm -f /tmp/script.tar.gz
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
 wait
-sleep 2
+killall -9 enigma2
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
